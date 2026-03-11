1a54161c07691b7a5575ec116412a6d43b97b628
Taurus-ZSZ <taurus_aries@163.com>
2026-03-11 18:23:21 +0800

    add example for cocotb and verilator sim use make

---
+++ 001_Prj/learn_cocotb_001/01_hdl/top.sv
@@ -0,0 +1,24 @@
+module top #(
+    parameter D_WIDTH = 8
+) (
+    input logic clk     ,
+    input logic rst_n   ,
+
+    input logic [D_WIDTH-1:0]   i_a,
+    input logic [D_WIDTH-1:0]   i_b,
+
+    output logic [D_WIDTH:0]    o_sum,
+    output logic                o_flag
+);
+
+    counter #(
+        .D_WIDTH(D_WIDTH)
+    ) u_counter(
+        .clk(clk),
+        .rst_n(rst_n),
+        .o_flag(o_flag)
+    );
+
+    assign o_sum = {i_a[D_WIDTH-1],i_a} + {i_b[D_WIDTH-1],i_b};
+
+endmodule
